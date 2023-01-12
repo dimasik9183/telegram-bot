015188e08b4c002278bc226d99b9f3917c40ae16
dimasik9183 <122044037+dimasik9183@users.noreply.github.com>
2023-01-12 17:01:47 +0200

    Add files via upload

---
+++ config.py
@@ -0,0 +1,20 @@
+
+
+TOKEN = '5946832249:AAGCJExJkvHCgpeKQX78MKAeFGV9f4dH6-w'
+
+
+
+STARTMSG = "Hi, i am translator bot."
+CHOOSEMSG = "Choose language"
+
+
+LANGDICT = {
+
+
+    'fr': 'french',
+    'it': 'italian',
+    'ja': 'japanese',
+    'ru': 'russian',
+    'uk': 'ukrainian'
+  }
+LANGUES = LANGDICT.keys()
